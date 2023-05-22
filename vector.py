b13d5b877f7150cc2ddc8bf615e242cf7949fa6d
Enrique Santos <santosenrique869@gmail.com>
2023-05-22 19:12:21 -0300

    refactor: update variables names

--- vector.py
+++ vector.py
@@ -4,23 +4,23 @@ import time
 import threading
 
 
-class VectorProcess:
-    def __init__(self, id: int, number_of_process: int = 4) -> None:
-        self.id = id
-        self.number_of_process = number_of_process
-        self.vector_clock = [0] * number_of_process
-        self.aux_vector_clock = [0] * number_of_process
+class Process:
+    def __init__(self, pid: int, total_process: int = 4) -> None:
+        self.pid = pid
+        self.total_process = total_process
+        self.vector_clock = [0] * total_process
+        self.aux_vector_clock = [0] * total_process
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind(('localhost', 5000 + id))
+        self.socket.bind(('localhost', 5000 + pid))
         self.socket.listen()
         self.receive_thread = threading.Thread(target=self.receive_messages)
         self.receive_thread.start()
 
-    def send_message(self, recipient_id: int) -> None:
-        self.vector_clock[self.id] += 1
-        message = (self.id, self.vector_clock)
+    def send_message(self, recipient_pid: int) -> None:
+        self.vector_clock[self.pid] += 1
+        message = (self.pid, self.vector_clock)
         recipient_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        recipient_socket.connect(('localhost', 5000 + recipient_id))
+        recipient_socket.connect(('localhost', 5000 + recipient_pid))
         recipient_socket.sendall(pickle.dumps(message))
         recipient_socket.close()
 
@@ -28,21 +28,21 @@ class VectorProcess:
         while True:
             conn = self.socket.accept()[0]
             data = conn.recv(4096)
-            sender_id, sender_vector_clock = pickle.loads(data)
+            sender_pid, sender_vector_clock = pickle.loads(data)
 
-            for p in range(self.number_of_process):
+            for p in range(self.total_process):
                 max_value = max(self.vector_clock[p], sender_vector_clock[p])
                 self.vector_clock[p] = max_value
 
-            print(f"processo: {self.id} vector de clock que tá vindo do processo {sender_id} {sender_vector_clock} já com o incremento do valor do evento")
-            print(f"processo: {self.id} vector de clock atualizado {self.vector_clock}")
+            print(f"processo: {self.pid} vector de clock que tá vindo do processo {sender_pid} {sender_vector_clock} já com o incremento do valor do evento")
+            print(f"processo: {self.pid} vector de clock atualizado {self.vector_clock}")
 
             conn.close()
 
 
-number_of_process = 4
-proc0, proc1, proc2, *_ = [VectorProcess(i, number_of_process)
-                           for i in range(number_of_process)]
+process_count = 4
+proc0, proc1, proc2, *_ = [Process(i, process_count)
+                           for i in range(process_count)]
 
 # process 0 send message to process two and wait 2 seconds
 proc0.send_message(1)
